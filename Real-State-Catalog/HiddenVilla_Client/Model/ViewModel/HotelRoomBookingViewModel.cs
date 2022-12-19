d23d2686d965ae75a57c5979fc3d4513b2f54301
Vik_toria <72942985+viktoria1108@users.noreply.github.com>
2022-12-19 03:43:07 +0200

    AddModelView

---
+++ Real-State-Catalog/HiddenVilla_Client/Model/ViewModel/HotelRoomBookingViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Models;
+
+namespace Real_State_Catalog_Client.Model.ViewModel
+{
+    public class HotelRoomBookingViewModel
+    {
+        public RoomOrderDetailsDTO orderDetails { get; set; }
+    }
+}
